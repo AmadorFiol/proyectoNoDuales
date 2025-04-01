86c64f8a1e429732e992c6eacd1d42ac8360c3c5
aleex825 <adehesad@alumnes.santjosepobrer.es>
2025-04-01 18:26:15 +0200

    Update back_users.py

--- back_users.py
+++ back_users.py
@@ -1,40 +1,59 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
-
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BibliotecaCapitan'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BibliotecaCapitan.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db= SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 class User(db.Model):
-    Id= db.Column(db.Integer, primary_key=True)
-    Fecha_nacimiento= db.Column(db.Date, nullable=True)
-    Nombre= db.Column(db.String(50), nullable=True)
-    Apellidos= db.Column(db.String(50), nullable=True)
-    Pais= db.Column(db.String(50), nullable=True)
-    Gmail= db.Column(db.String(50), nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    fecha_nacimiento = db.Column(db.Date, nullable=True)
+    nombre = db.Column(db.String(50), nullable=True)
+    apellidos = db.Column(db.String(50), nullable=True)
+    pais = db.Column(db.String(50), nullable=True)
+    gmail = db.Column(db.String(50), nullable=True)
 
-
-    #Add user
 @app.route('/signup', methods=['GET', 'POST'])
 def add_user():
     if request.method == 'POST':
         id = request.form.get('id')
-        Fecha_nacimiento = request.form.get('fecha_nacimiento')
-        Nombre = request.form.get('nombre')
-        Apellidos = request.form.get('apellidos')            
-        Pais= request.form.get('pais')
-        Gmail= request.form.get('gmail')
-        new_user = User(id=id, fecha_nacimiento=Fecha_nacimiento, Nombre=Nombre, Apellidos=Apellidos, Pais=Pais, Gmail=Gmail)
+        fecha_nacimiento = request.form.get('fecha_nacimiento')
+        nombre = request.form.get('nombre')
+        apellidos = request.form.get('apellidos')            
+        pais = request.form.get('pais')
+        gmail = request.form.get('gmail')
+        new_user = User(id=id, fecha_nacimiento=fecha_nacimiento, nombre=nombre, apellidos=apellidos, pais=pais, gmail=gmail)
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('get_libros'))
-        
-        # return render_template('new_libro.html')
+        return redirect(url_for('get_libros')) 
+    return render_template('signup.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    if request.method == "POST":
+        gmail = request.form.get('gmail')
+        user = User.query.filter_by(gmail=gmail).first()
+        if user:
+           #Logica sintaxis del user
+            return redirect(url_for('user_profile', user_id=user.id)) 
+        else:
+            return 'Usuario no encontrado', 404
+    return render_template('login.html') 
+
+@app.route('/user/<int:user_id>')
+def user_profile(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return jsonify({
+            "id": user.id, 
+            "Nombre": user.nombre, 
+            "Apellidos": user.apellidos, 
+            "Gmail": user.gmail, 
+            "Pais": user.pais, 
+            "Fecha_nacimiento": user.fecha_nacimiento.strftime("%Y-%m-%d")
+        })
+    else:
+        return jsonify({"error": "Usuario no encontrado"}), 404
